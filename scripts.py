a0659cde68e079efe39083184cc78e303996e5b1
Tim Xie <pdt.xie@gmail.com>
2023-10-17 19:18:50 -0700

    test

--- scripts.py
+++ scripts.py
@@ -2,5 +2,8 @@ from data.datasets import CIFAR10Dataset
 
 
 def visualise():
-    x = CIFAR10Dataset() 
-    print(x)
+    x = CIFAR10Dataset()
+    for a, b in x:
+        print(a, b)
+
+visualise()
