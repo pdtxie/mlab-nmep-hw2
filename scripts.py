5db2ff8f2d3591ec9405ad7c9dfff4a668e74d25
Tim Xie <pdt.xie@gmail.com>
2023-10-17 18:47:53 -0700

    fuck everything

---
+++ scripts.py
@@ -0,0 +1,6 @@
+from data.datasets import CIFAR10Dataset
+
+
+def visualise():
+    x = CIFAR10Dataset() 
+    print(x)
--- scripts/visualise.py
+++
@@ -1,7 +0,0 @@
-from ..data import datasets
-
-
-def visualise():
-    print("HERE")
-    cifar10_dataset = datasets.CIFAR10Dataset()
-    print(cifar10_dataset)
