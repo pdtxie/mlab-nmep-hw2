5c1d17f08a5856946350992e854e8a4e02773980
Tim Xie <pdt.xie@gmail.com>
2023-10-23 00:18:27 -0700

    add padding

--- models/resnet.py
+++ models/resnet.py
@@ -36,11 +36,11 @@ class ResNetBlock(nn.Module):
         and after the second batchnorm gets added to the shortcut.
         """
         self.features = [
-            nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=3, stride=stride),
+            nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=3, stride=stride, padding=1),
             nn.BatchNorm2d(out_channels),
             nn.ReLU(),
 
-            nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=3, stride=1),
+            nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=3, stride=1, padding=1),
             nn.BatchNorm2d(out_channels),
             nn.ReLU()
         ]
@@ -51,7 +51,7 @@ class ResNetBlock(nn.Module):
 
         if out_channels != in_channels or stride != 1:
             self.shortcut = [
-                nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=1, stride=stride),
+                nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=1, stride=stride, padding=1),
                 nn.BatchNorm2d(out_channels)
             ]
 
