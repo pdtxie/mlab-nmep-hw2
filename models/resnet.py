9dcdb99a79bd7ed2b109916d285b763879edb17b
Tim Xie <pdt.xie@gmail.com>
2023-10-23 01:37:57 -0700

    fix

--- models/resnet.py
+++ models/resnet.py
@@ -38,20 +38,19 @@ class ResNetBlock(nn.Module):
         super().__init__()
 
         self.features = nn.Sequential(
-            nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=3, stride=stride, padding=1),
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False),
             nn.BatchNorm2d(out_channels),
             nn.ReLU(),
 
-            nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False),
             nn.BatchNorm2d(out_channels))
 
         self.shortcut = nn.Sequential()
 
         if out_channels != in_channels or stride != 1:
             self.shortcut = nn.Sequential(
-                nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=1, stride=stride, padding=1),
-                nn.BatchNorm2d(out_channels)
-            )
+                nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride, padding=1, bias=False),
+                nn.BatchNorm2d(out_channels))
 
     def forward(self, x):
         """
@@ -64,7 +63,7 @@ class ResNetBlock(nn.Module):
         x1 = self.features(x)
         x2 = self.shortcut(x)
 
-        return nn.ReLU(x1 + x2)
+        return F.relu(x1 + x2)
 
 
 class ResNet18(nn.Module):
@@ -72,12 +71,7 @@ class ResNet18(nn.Module):
         num_classes = num_classes
         super(ResNet18, self).__init__()
         self.in_channels = 64
-        self.conv1 = nn.Conv2d(in_channels=3,
-                               out_channels=64,
-                               kernel_size=3,
-                               stride=1,
-                               padding=1,
-                               bias=False)
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=64, kernel_size=3, stride=1, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(64)
         self.layer1 = self.make_block(out_channels=64, stride=1)
         self.layer2 = self.make_block(out_channels=128, stride=2)
@@ -87,9 +81,11 @@ class ResNet18(nn.Module):
 
     def make_block(self, out_channels, stride):
         layers = []
+
         for stride in [stride, 1]:
             layers.append(ResNetBlock(self.in_channels, out_channels, stride))
             self.in_channels = out_channels
+
         return nn.Sequential(*layers)
 
     def forward(self, x):
